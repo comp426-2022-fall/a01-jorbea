9df1982914b31dc3122e7801f4ddf27a59b65115
Jordan Bean <jorbea@live.unc.edu>
2022-09-01 16:42:57 -0400

    Combined minimist requirement and arg process

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const fs = require('fs');
 const minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = process.argv.slice(2);
+const args = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.a
-const port = args[0] || process.env.PORT || 3000;
+const port = args.port || process.env.PORT || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
