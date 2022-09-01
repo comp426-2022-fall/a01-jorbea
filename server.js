988777a39b9e72687dd86c0dfa88d3246c813e44
Jordan Bean <jorbea@live.unc.edu>
2022-09-01 16:27:13 -0400

    Modified port constant

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const args = process.argv.slice(2);
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.a
-const port = args || process.env.PORT || 3000;
+const port = args[0] || process.env.PORT || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
