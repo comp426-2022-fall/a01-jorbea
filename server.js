c97c11ef6ed6e17ea12dbf3078b9dbfe4a36f5b2
Jordan Bean <jorbea@live.unc.edu>
2022-09-01 21:43:31 -0400

    Reset index.html

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const port = args.port || process.env.PORT || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile('./public/index.html', 'utf8', (err) => {
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
 	
 	// If there is an error, put it on the console error and return. 
 	// Do not be nice about exiting.
